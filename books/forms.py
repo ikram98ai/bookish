5cc83306f5491adc124f008452cafe9c3fa91ad4
unknown <ikram9820khan@gmail.com>
2023-02-14 13:39:52 +0500

    db changed to sqlite

--- books/forms.py
+++ books/forms.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from django import forms
 from .models import Book
 class BookCreationForm(forms.ModelForm):
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,14 +5,20 @@ SECRET_KEY = 'django-insecure-6j2@8g)ygvsiuvnh1w8cs&o)k**r'
 
 ALLOWED_HOSTS = ["*"]
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432
+#     }
+# }
