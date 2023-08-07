045089acecd85bde5a124b209d77ff6dea49a7b9
ikram9820 <ikram9820khan@gmail.com>
2023-08-08 01:11:18 +0500

    requesting update_visibility and add_to_fav with htmx

--- books/views.py
+++ books/views.py
@@ -11,20 +11,6 @@ from .forms import BookCreationForm, BookUpdateForm
 from .ask_pdf import ask_pdf,create_vectorstore
 import fitz
 
-class BookListView(ListView):
-    model = Book
-    template_name = 'books/book_list.html'
-    context_object_name = 'books'
-    paginate_by = 12
-
-    def get_queryset(self):
-        return Book.objects.filter(is_visible=True).order_by('-posted_at').select_related('user')
-
-
-class BookDetailView(DetailView):
-    model= Book
-    template_name='books/book_detail.html'
-    context_object_name= 'book'
 
 
 def cover(pdf):
@@ -48,6 +34,73 @@ def ask_question(request,pk):
     answer =ask_pdf(question,vectorstore)["answer"]
     return render(request,"partial/answer.html",{"answer":answer})
  
+def update_visibility(request,pk):
+    book = Book.objects.get(id=pk)
+    book.is_visible = not book.is_visible
+    book.save()
+    print(f"visibility of {pk}:",book.is_visible)
+    visibility = "Private" if book.is_visible else "Public"
+    return render(request,'partial/visibility.html',{"visibility":visibility})
+
+
+def get_favorite(request):
+    user = request.user if request.user.is_authenticated else None
+    if request.user.is_authenticated:
+        try:
+            fav = Favorite.objects.get(user=user)
+        except:
+            fav = None
+    else:
+        try:
+            fav = Favorite.objects.get(
+                id=request.session.get('fav_uuid', None))
+        except:
+            fav = None
+    if not fav:
+        fav = Favorite.objects.create(user=user)
+        request.session['fav_uuid'] = str(fav.id)
+    return fav
+
+
+def get_fav_book_list(request):
+    fav = get_favorite(request)
+    fav_books = FavoriteBook.objects.filter(
+        favorite=fav).only('book').select_related('book__user')
+    books = []
+    for fav_book in fav_books:
+        books.append(fav_book.book)
+    return render(request, 'books/fav.html', {'books': books})
+
+
+def add_book_to_fav(request, pk):
+    fav = get_favorite(request)
+    try:
+        clss = " link-dark"
+        fav_book = FavoriteBook.objects.get(favorite=fav, book_id=pk)
+        fav_book.delete()
+
+    except FavoriteBook.DoesNotExist:
+        clss = "-fill link-danger" 
+        FavoriteBook.objects.create(favorite=fav, book_id=pk)
+    return render(request,"partial/add_to_fav.html",{"clss":clss})
+
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Book.objects.filter(is_visible=True).order_by('-posted_at').select_related('user')
+
+
+class BookDetailView(DetailView):
+    model= Book
+    template_name='books/book_detail.html'
+    context_object_name= 'book'
+
 
 class BookCreateView(LoginRequiredMixin, CreateView):
     model = Book
@@ -92,19 +145,6 @@ class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return False
 
 
-class BookVisibilityUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Book
-    template_name = "books/book_form.html"
-    fields = ['is_visible']
-    success_url = reverse_lazy('my_profile')
-
-    def test_func(self):
-        book = self.get_object()
-        if self.request.user == book.user:
-            return True
-        return False
-
-
 class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Book
     success_url = reverse_lazy('my_profile')
@@ -116,45 +156,6 @@ class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
-
-def get_favorite(request):
-    user = request.user if request.user.is_authenticated else None
-    if request.user.is_authenticated:
-        try:
-            fav = Favorite.objects.get(user=user)
-        except:
-            fav = None
-    else:
-        try:
-            fav = Favorite.objects.get(
-                id=request.session.get('fav_uuid', None))
-        except:
-            fav = None
-    if not fav:
-        fav = Favorite.objects.create(user=user)
-        request.session['fav_uuid'] = str(fav.id)
-    return fav
-
-
-def get_fav_book_list(request):
-    fav = get_favorite(request)
-    fav_books = FavoriteBook.objects.filter(
-        favorite=fav).only('book').select_related('book__user')
-    books = []
-    for fav_book in fav_books:
-        books.append(fav_book.book)
-    return render(request, 'books/fav.html', {'books': books})
-
-
-def add_book_to_fav(request, pk):
-    fav = get_favorite(request)
-
-    try:
-        fav_book = FavoriteBook.objects.get(favorite=fav, book_id=pk)
-        fav_book.delete()
-    except FavoriteBook.DoesNotExist:
-        FavoriteBook.objects.create(favorite=fav, book_id=pk)
-    return redirect('book_list')
 
 
 class ProfileListView(ListView):
